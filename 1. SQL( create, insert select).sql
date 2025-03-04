ef61f3ef7bd5a1058cb3b5cfd0763485b8ca449a
Md. Abdur Rahman <abdur.rahman131054@gmail.com>
2025-03-04 23:33:32 +0600

    Add files via upload

---
+++ 1. SQL( create, insert select).sql
@@ -0,0 +1,4 @@
+INSERT INTO "sales"."orders" ("order_id", "cust_name", "amount") VALUES
+(123, 'Abc', 55.6);
+INSERT INTO "sales"."orders" ("order_id", "cust_name", "amount") VALUES
+(124, 'BCD', 54.6);
