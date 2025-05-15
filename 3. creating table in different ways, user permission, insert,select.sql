25e7a07d095522bf5b9a9251bae933a61ceab024
Md. Abdur Rahman <abdur.rahman131054@gmail.com>
2025-05-15 20:49:10 +0600

    3. creating table in different ways, user permission, insert,select.sql

--- 3. creating table in different ways, user permission, insert,select.sql
+++ 3. creating table in different ways, user permission, insert,select.sql
@@ -2,7 +2,7 @@
 create table sales.new_tbl (id int, name text);
 drop table sales.new_tbl;
 
---creating temporary table and drop table
+--creating temporary table and temporary table and drop table
 create temporary table  new_tbl (id int, name text);
 
 INSERT INTO new_tbl
@@ -21,8 +21,6 @@ create table new_tbl
 (id int, name text, creation_date date)
 partition by range(creation_date);
 
-create table new_tbl_2025_03_08 partition of new_tbl default;
-
 --inserting data into partion table
 insert into new_tbl
 (id, name, creation_date)
@@ -30,7 +28,7 @@ VALUES
 (1, 'abcd', '2025-03-7'),
 (2, 'bcd', '2025-03-8');
 
-
+create table new_tbl_2025_03_08 partition of new_tbl default;
 
 --want to see in information schema have how many tables
 
@@ -47,10 +45,12 @@ drop index order_id_idx;
 create table sales.new_tbl (id int primary key , name text)
 
 --create tabele employees
-create table sales.employees (id serial primary key, emp_name VARCHAR, emp_id int, created_at date);--here employees mentioned as id 
+create table sales.employees (id serial primary key, emp_name VARCHAR, emp_id int, created_at date);
+--here employees mentioned as id 
 drop TABLE sales.employees ;
 
-create table sales.departments(id serial primary key, dept_name VARCHAR, emp_id serial, created_at date);--here employees id mentioned as emp_id, but kept data type same , so we can use different columns names in primary and foreign key but data type should be same
+create table sales.departments(id serial primary key, dept_name VARCHAR, emp_id serial, created_at date);
+--here employees id mentioned as emp_id, but kept data type same , so we can use different columns names in primary and foreign key but data type should be same
 
 
 --as we not define foriegn key on departments table before creating departments table , now we can modify this table 
